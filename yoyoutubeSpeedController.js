8f34dc603fc9f7ec441452eaa8e32dc4d519e337
Rohith Goud Panjala <34999805+rohithgoud30@users.noreply.github.com>
2023-10-07 17:53:38 -0400

    youtubeSpeedController.js

---
+++ yoyoutubeSpeedController.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name         Speed up video with Overlay
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Display video speed as overlay on YouTube player
+// @author       rohithgoud30
+// @match        https://www.youtube.com/watch?*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    var speed = 1;
+    var overlay = document.createElement('div');
+    overlay.style.position = 'fixed';
+    overlay.style.bottom = '10px';
+    overlay.style.right = '10px';
+    overlay.style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
+    overlay.style.color = '#000';
+    overlay.style.fontWeight = 'bold';
+    overlay.style.fontSize = '18px';
+    overlay.style.padding = '10px';
+    overlay.style.zIndex = '9999';
+    overlay.style.transition = 'visibility 1s ease';
+    overlay.style.visibility = 'hidden';
+    document.body.appendChild(overlay);
+
+    function updateSpeed() {
+        var speedText = 'Speed: ' + speed.toFixed(2);
+        var highlightedSpeedText = speedText.replace(/(\d+\.\d+)/, '<span style="color: #fdb515;">$1</span>');
+        
+        overlay.innerHTML = highlightedSpeedText;
+        
+        var authorTag = document.createElement('div');
+        authorTag.textContent = 'Script by @rohithgoud30';
+        authorTag.style.fontSize = 'small';
+        overlay.appendChild(authorTag);
+        
+        overlay.style.visibility = 'visible';
+        setTimeout(function() {
+            overlay.style.visibility = 'hidden';
+            overlay.removeChild(authorTag);
+        }, 1000);
+    }
+
+    document.addEventListener('keydown', function(e) {
+        if (e.ctrlKey && e.key === '.') {
+            speed += 0.25;
+        } else if (e.ctrlKey && e.key === ',') {
+            speed -= 0.25;
+        }
+
+        if (speed < 0.25) {
+            speed = 0.25;
+        }
+
+        document.getElementsByClassName('html5-main-video')[0].playbackRate = speed;
+        updateSpeed();
+    });
+})();
